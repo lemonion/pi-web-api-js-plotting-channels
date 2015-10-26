4d57607987850f501dec3b1c57d25fa6f67b3d00
Daphne Ng <dng@osisoft.com>
2015-10-26 10:44:23 -0400

    Added initial project

---
+++ pi-web-api-js-plotting-channels/Scripts/app.js
@@ -0,0 +1,184 @@
+﻿var piTagWebIds = {};
+var plotData = [[]];
+var width = "800";
+
+// General error call back
+var errorCallBack = function (xhr) {
+    console.log(xhr.responseText);
+}
+
+// Flot plot options
+var options = {
+    xaxis: {
+        mode: "time",
+        timeformat: "%Y/%m/%d %H:%M:%S",
+        timezone: "browser"
+    }
+};
+
+// Reset buttons, text boxes and plot in the plot view
+var resetPlotView = function () {
+    $('#tagmask-text').val('');
+    $('#tag-select').empty();
+    $('#time-interval-text').val('');
+    $('#plot-btn').attr('disabled', 'disabled');
+    $('#stop-btn').attr('disabled', 'disabled');
+    $('#plot').empty();
+};
+
+// Get the plot start time based on current time and plot time interval
+var getPlotStartTime = function (intervalUnit, intervalVal) {
+    var ret = new Date();
+    switch (intervalUnit.toLowerCase()) {
+        case 'h': ret.setTime(ret.getTime() - intervalVal * 3600000); break;
+        case 'm': ret.setTime(ret.getTime() - intervalVal * 60000); break;
+        case 's': ret.setTime(ret.getTime() - intervalVal * 1000); break;
+        default: ret = undefined; break;
+    }
+    return ret;
+};
+
+// Test username/password and go to plot screen
+$('#go-to-plot-btn').click(function () {
+    // Store username and password
+    var username = $("#username").val();
+    var password = $("#password").val();
+    piwebapi.SetCredentials(username, password);
+
+    // Check authentication
+    var authSuccessCallBack = function (data, statusMessage, statusObj) {
+        if (statusObj.status == 200) {
+            $('#auth-view-mode').hide();
+            $('#plot-view-mode').show();
+        }
+    };
+    var authErrorCallBack = function (data) {
+        if (data.status == 401) {
+            alert("Invalid username and password.");
+        }
+        else {
+            alert("Error during validation.");
+        }
+    };
+    piwebapi.Authorize(authSuccessCallBack, authErrorCallBack);
+});
+
+// Go back to login screen
+$("#back-btn").click(function () {
+    $("#username").val('');
+    $("#password").val('');
+
+    $("#plot-view-mode").hide();
+    $("#auth-view-mode").show();
+
+    piwebapi.Reset();
+    resetPlotView();
+});
+
+// Search for tags using tag mask
+$('#search-btn').click(function (data) {
+    $('#plot-btn').attr('disabled', 'disabled');
+    $('#tag-select').empty();
+    var tagMask = $('#tagmask-text').val();
+    if (tagMask.length > 0) {
+        var populateTags = function (tags) {
+            $.each(tags, function (name, webId) {
+                $('#tag-select').append("<option value=\"" + name + "\">" + name + "</option>");
+            });
+            piTagWebIds = tags;
+            $('#plot-btn').removeAttr('disabled');
+        };
+        var error = function () {
+            alert("Error during tag search.");
+        };
+        piwebapi.GetTagList(tagMask, populateTags, error, errorCallBack);
+    }
+});
+
+// Start plot
+$('#plot-btn').click(function (data) {
+    var tag = $('#tag-select option:selected').text();
+    var timeInterval = $('#time-interval-text').val();
+    var timeIntervalUnit = $('#time-interval-unit option:selected').val();
+
+    if (tag.length > 0) {
+        // Get initial values
+        if (timeInterval.length <= 0 || isNaN(timeInterval)) {
+            timeInterval = 1;
+            $('#time-interval-text').val(timeInterval);
+        }
+        var startTime = "*-" + timeInterval + timeIntervalUnit;
+
+        var successCallBack = function (values) {
+            plotData = [[]];
+            for (var i = 0; i < values.Items.length; i++) {
+                if (values.Items[i].Good) {
+                    plotData[0].push([new Date(values.Items[i].Timestamp).getTime(), values.Items[i].Value]);
+                }
+            }
+            $.plot($('#plot'), plotData, options);
+        };
+
+        piwebapi.GetValues(piTagWebIds[tag], startTime, width, successCallBack, errorCallBack)
+
+        // Open channel
+        var messageCallBack = function (event) {
+            var values = JSON.parse(event.data);
+            var plotStartTime = getPlotStartTime(timeIntervalUnit, timeInterval);
+
+            // Remove old values before new start time
+            var removeCount = 0;
+            while (plotData[0][removeCount][0] < plotStartTime.getTime()) {
+                removeCount++;
+            }
+            plotData[0].splice(0, removeCount);
+
+            // Add new values
+            for (var i = 0; i < values.Items[0].Items.length; i++) {
+                if (values.Items[0].Items[i].Good) {
+                    var timestamp = new Date(values.Items[0].Items[i].Timestamp);
+                    if (timestamp.getTime() >= plotStartTime.getTime()) {
+                        plotData[0].push([timestamp.getTime(), values.Items[0].Items[i].Value]);
+                    }
+                }
+            }
+
+            // Sort array
+            plotData[0].sort(function (a, b) {
+                if (a[0] === b[0]) {
+                    return 0;
+                }
+                else {
+                    return (a[0] < b[0]) ? -1 : 1;
+                }
+            });
+
+            // Plot
+            $.plot($('#plot'), plotData, options);
+        };
+
+        var errorCallBack = function () {
+            alert("Error getting updates.");
+        };
+
+        var openCallBack = function () {
+            $('#search-btn').attr('disabled', 'disabled');
+        };
+
+        var closeCallBack = function () {
+            $('#search-btn').removeAttr('disabled');
+        };
+
+        piwebapi.OpenChannel(piTagWebIds[tag], openCallBack, errorCallBack, messageCallBack, closeCallBack);
+
+        $('#plot-btn').attr('disabled', 'disabled');
+        $('#stop-btn').removeAttr('disabled');
+    }
+});
+
+// Stop update
+$('#stop-btn').click(function (data) {
+    piwebapi.CloseChannel();
+    $('#plot-btn').removeAttr('disabled');
+    $('#stop-btn').attr('disabled', 'disabled');
+});
---
+++ pi-web-api-js-plotting-channels/Scripts/piwebapi_wrapper.js
@@ -0,0 +1,133 @@
+﻿var basePIWebAPIUrl = "https://YourPIWebAPIServer/piwebapi";
+var basePIWebAPIChannelUrl = "wss://YourPIWebAPIServer/piwebapi";
+var piDataArchiveName = "YourPIDataArchive";
+
+// PI Web API wrapper module
+var piwebapi = (function () {
+    var currentUserName = null;
+    var currentPassword = null;
+    var webSocket = null;
+
+    // Send ajax request
+    var processJsonContent = function (url, type, data, successCallBack, errorCallBack) {
+        return $.ajax({
+            url: encodeURI(url),
+            type: type,
+            data: data,
+            contentType: "application/json; charset=UTF-8",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", makeBasicAuth(currentUserName, currentPassword));
+            },
+            success: successCallBack,
+            error: errorCallBack
+        });
+    };
+
+    // Return authorization header value for basic authentication
+    var makeBasicAuth = function (user, password) {
+        var tok = user + ':' + password;
+        var hash = window.btoa(tok);
+        return "Basic " + hash;
+    };
+
+    // Batch demonstration: get list of tags based on particular PI Data Archive and tag mask
+    var getTagListRequest = function (piDataArchive, tagMask, successCallBack, errorCallBack) {
+        var url = basePIWebAPIUrl + "/batch";
+        var data = {};
+        data["1"] = {
+            "Method": "GET",
+            "Resource": basePIWebAPIUrl + "/dataservers?name=" + piDataArchive
+        };
+        data["2"] = {
+            "Method": "GET",
+            "Resource": "{0}?nameFilter=" + tagMask,
+            "Parameters": [
+              "$.1.Content.Links.Points"
+            ],
+            "ParentIds": [
+              "1"
+            ]
+        }
+        return processJsonContent(url, "POST", JSON.stringify(data), successCallBack, errorCallBack);
+    };
+
+    // Normal HTTP request: get plot values based on webId
+    var getValuesRequest = function (tagWebId, startTime, pixels, successCallBack, errorCallBack) {
+        var url = basePIWebAPIUrl + "/streams/" + tagWebId + "/plot?intervals=" + pixels;
+        if (startTime != null) {
+            url += "&starttime=" + startTime;
+        }
+        return processJsonContent(url, "GET", null, successCallBack, errorCallBack);
+    };
+
+    // Channels demonstration: open web socket based on webId
+    var openStreamChannel = function (tagWebId, channelOpenCallBack, channelErrorCallBack, channelMessageCallBack, channelCloseCallBack) {
+        var url = basePIWebAPIChannelUrl + "/streams/" + tagWebId + "/channel";
+        webSocket = new WebSocket(encodeURI(url));
+        webSocket.onopen = channelOpenCallBack;
+        webSocket.onerror = channelErrorCallBack;
+        webSocket.onmessage = channelMessageCallBack;
+        webSocket.onclose = channelCloseCallBack;
+    };
+
+    // Channels demonstration: close web socket
+    var closeStreamChannel = function () {
+        if (webSocket != null) {
+            webSocket.close();
+        }
+    };
+
+    // Publicly available methods
+    return {
+        // Store current username and password
+        SetCredentials: function (user, password) {
+            currentUserName = user;
+            currentPassword = password;
+        },
+
+        // Test basic authentication
+        Authorize: function (successCallBack, errorCallBack) {
+            return processJsonContent(basePIWebAPIUrl, 'GET', null, successCallBack, errorCallBack);
+        },
+
+        // Reset username/password and web socket
+        Reset: function () {
+            currentUserName = null;
+            currentPassword = null;
+            webSocket.close();
+            webSocket = null;
+        },
+
+        // Get the list of tags based on tagmask
+        GetTagList: function (tagMask, processFunction, errorFunction, errorCallBack) {
+            var successCallBack = function (data) {
+                var filteredTags = {};
+                if (data["1"].Status == 200 && data["2"].Status == 200) {
+                    for (var i = 0; i < data["2"].Content.Items.length; i++) {
+                        filteredTags[data["2"].Content.Items[i].Name] = data["2"].Content.Items[i].WebId;
+                    }
+                    processFunction(filteredTags);
+                }
+                else {
+                    errorFunction();
+                }
+            };
+            getTagListRequest(piDataArchiveName, tagMask, successCallBack, errorCallBack);
+        },
+
+        // Get plot values based on webId
+        GetValues: function (tagWebId, startTime, pixels, successCallBack, errorCallBack) {
+            getValuesRequest(tagWebId, startTime, pixels, successCallBack, errorCallBack);
+        },
+
+         // Open channel for a stream
+        OpenChannel: function (tagWebId, channelOpenCallBack, channelErrorCallBack, channelMessageCallBack, channelCloseCallBack) {
+            openStreamChannel(tagWebId, channelOpenCallBack, channelErrorCallBack, channelMessageCallBack, channelCloseCallBack);
+        },
+
+        // Close existing opened channel
+        CloseChannel: function () {
+            closeStreamChannel();
+        }
+    }
+})();
